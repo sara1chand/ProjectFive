564b33fc38b5eea04775e9cd40e4f78eda9d590b
chand <chand@DESKTOP-NB9NGI7>
2021-11-24 19:59:14 -0500

    Implemented add method!

--- src/five/DemoFive.java
+++ src/five/DemoFive.java
@@ -5,6 +5,12 @@ public class DemoFive {
 	public static void main(String[] args) {
 		
 		System.out.println("Demo Five...");
+		
+	}
+	
+	public static void addnum(int a, int b) {
+		
+		System.out.println(a+b);
 	}
 
 }
